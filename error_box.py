fff16d9bc32f0c8116dae00112856f3494d3568d
FoxReven <patryk.urbani9@gmal.com>
2023-12-03 16:21:23 +0100

    new program

---
+++ error_box.py
@@ -0,0 +1,59 @@
+import time
+import os
+import sys
+class Hellp_box:
+     
+     def __init__(self,error) -> None: # funkcja która dziedziczy bład z innej  classy
+          self.error=error
+          self.boot="Robert"
+
+
+     def menu(self)->str:  #przedstawienie programu 
+          os.system("cls")
+          print(f"{self.boot} > witam cie w dziale pomocy drogi użytkowniku widze ze wydżył sie jakiś problem" )
+    
+     def loding(self)->None: # funkcja która daje na animacje Lodingu .... 
+        print("Loading")     
+        animation = "|/-\\"
+        for i in range(20):
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            sys.stdout.flush()
+            time.sleep(0.1) 
+
+     def error_box(self)->str: # wyszukiwarka błedów 
+          
+          match self.error:
+               
+               case "E40":
+                    os.system("cls")
+                    print(f"{self.boot}> Wychodzi na to że powstała pomyłka w klawiszach \n  "
+                          f"{self.boot} > za  5 sekund przeniose cie  do Menu Głównego ")
+                    time.sleep(5)
+                    ret=Return()
+                    ret.loding()
+                    ret._return_Menu()
+
+
+
+
+
+
+
+
+class Return:
+      
+      def loding(self)->None:
+        os.system("cls")
+        print("Loading")     
+        animation = "|/-\\"
+        for i in range(20):
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            sys.stdout.flush()
+            time.sleep(0.1) 
+
+      def _return_Menu(self)->None:
+            
+              from main import _menu_go
+              _menu_go()
+
+              
---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import time
+from error_box import *
+
+def __Start_app()->None: # Funkcja prywatna która przyjmuje obiekt menu głownego 
+     os.system("cls")
+     start=menu_start()
+     start._menu()
+     start._opcion()
+     start._key_opicon()
+
+def _menu_go(): #odczyt funcki prywatnej 
+       __Start_app()
+
+class menu_start: # obiekt 
+     
+      def _menu(self)->str: 
+             print("\t\t\tWitamy W aplikajci MXr \n" 
+                   "Zaraz Wyswietlą sie opcje dowyboru")
+             
+
+      def _opcion(slef)->None: # lista poleceni 
+            time.sleep(1)
+            lista_opcion={"Nowe Zamówienie ", "Podgląd Zamówienia" , "Dział Pomocy " } 
+
+            for x , y in enumerate(lista_opcion):
+
+                  print(x+1,y)
+      
+      
+
+
+
+      def _key_opicon(self)->int:
+
+            try : # klawisz 
+                  key=int(input("Kliknij odpowieni kalwisz : "))
+
+            except ValueError : # wyłapywanie błednego klawiszuu 
+                   os.system("cls")
+                   NameError="E40" 
+                   print(f"wystąpił  bład :  {NameError} zaraz program cie przeniesie do działu pomocy ")
+                   error="E40"
+                   time.sleep(2)
+                   errorbox=Hellp_box(error)
+                   errorbox.menu()
+                   errorbox.loding()
+                   errorbox.error_box()
+
+            else: # przenoszenie do odpowiedniego działu 
+                  os.system('cls')
+                  print("Dział w budowie")                               
+            
+
+        
+
+
+_menu_go()
